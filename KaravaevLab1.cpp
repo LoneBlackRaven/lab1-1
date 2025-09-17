092444bc7f4ee3c1bf8f4b19431b8e7055900503
Arseniy <Senya.ka.to@mail.ru>
2025-09-17 11:35:35 +0300

    Добавлен основной код

--- KaravaevLab1.cpp
+++ KaravaevLab1.cpp
@@ -1,20 +1,94 @@
-﻿// KaravaevLab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+struct Pipe {
+    string name;
+    float length;
+    int diametr;
+    bool repair;
+};
+
+struct Compress {
+    string name;
+    int count;
+    int count_working;
+    string classification;
+    bool working;
+};
+
+void Menu(Pipe t, Compress c) {
+    while (true)
+    {
+        cout << "\nChoose type:\n 1. Pipe \n 2. Compress\n 3. Show All\n";
+        int type;
+        cin >> type;
+        switch (type)
+        {
+        case 1:
+            cout << "Choose option:\n 1. Add pipe \n 2. Pipe lenght\n 3. Pipe diametr\n 4. Repairing\n";
+            int option;
+            cin >> option;
+            switch (option)
+            {
+            case 1:
+                cout << "Enter pipe name:";
+                cin >> t.name;
+                break;
+            case 2:
+                cout << "Enter pipe lenght (km): ";
+                cin >> t.length;
+                break;
+            case 3:
+                cout << "Enter pipe diametr (mm): ";
+                cin >> t.diametr;
+                break;
+            case 4:
+                cout << "Is pipe on repair? (0 - no, 1 - yes)";
+                cin >> t.repair;
+                break;
+            }
+            break;
+        case 2:
+            cout << "Choose option:\n 1. Add CS \n 2. Quantity of CS \n 3. Quantity working\n 4. Classification 5. Working\n";
+            int option2;
+            cin >> option2;
+            switch (option2)
+            {
+            case 1:
+                cout << "Enter CS name: ";
+                cin >> c.name;
+                break;
+            case 2:
+                cout << "Enter quantity of enterprises: ";
+                cin >> c.count;
+                break;
+            case 3:
+                cout << "Enter quantity of working enterprises: ";
+                cin >> c.count_working;
+                break;
+            case 4:
+                cout << "Enter classification: ";
+                cin >> c.classification;
+                break;
+            case 5:
+                cout << "Is CS working? (0 - no, 1 - yes) ";
+                cin >> c.working;
+                break;
+            }
+            break;
+        case 3:
+            cout << "\nPipe parameters: \n" << "Name: " << t.name << "\nLength: " << t.length << "\nDiametr: " << t.diametr << "\nOn repair: " << t.repair;
+            cout << "\nCS parameters: \n" << "Name: " << c.name << "\nQuantity: " << c.count << "\nOn working: " << c.count_working << "\nClassification: " << c.classification << "\nWorking : " << c.working;
+            break;
+        }
+    };
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Pipe truba;
+    Compress ceh;
+    Menu(truba, ceh);
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
